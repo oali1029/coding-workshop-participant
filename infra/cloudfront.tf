75cd788f70b17cce0f4bd14100724e2e07710f33
Omar Haifa <134441360+oali1029@users.noreply.github.com>
2026-09-23 20:25:37 -0400

    Fix CloudFront SPA routing

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -40,6 +40,13 @@ resource "aws_cloudfront_distribution" "this" {
   }
 
   custom_error_response {
+    error_code            = 403
+    error_caching_min_ttl = 0
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
+  custom_error_response {
     error_code            = 404
     error_caching_min_ttl = 300
     response_code         = 200
